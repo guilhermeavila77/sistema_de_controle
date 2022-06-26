d1407cbace6a1fc3286ad26c8e601d415ab233a4
Guilherme Ávila <90806129+guilhermeavila77@users.noreply.github.com>
2022-06-26 00:11:14 -0300

    Criação do arquivo
    Criação do arquivo .py para o sistema utilizando os conceitos já estruturados no notebook do jupyter

---
+++ sistema.py
@@ -0,0 +1,42 @@
+# Importar bibliotecas
+
+# Função de buscar a planilha e criar o data frame com o nome tabela
+
+# Adicionar uma nova linha com inputs do usuario
+# Pega as informações de cada linha
+# Formata as variaveis inteiras
+# Adiciona a linha
+# Salva a planilha e atualizar o data frame
+
+# Calcula o lucro
+# Atualizar e salvar o data frame
+
+# Altera uma informação solicitada
+# Pede a coluna e a linha
+# Pede a coluna e passar o nome
+# Pede a linha e passar o numero da linha
+# Formata a linha com inteiro
+# Pede a alteração
+# Salva a planilha
+
+# Escluir uma linha
+# Pede e formata a linha que deseja ser excluida
+# Mostra a linha excluida
+# Exclui a linha da tabela
+# Salva a tabela
+
+# Cria as variaveis utilizadas para a busca e as "seta" como zero
+# As buscas são
+# Maior faturamento, maior despesa, maior lucro
+
+# Laços de repetição que procuram as informações
+# A cada linha ele verifica se o valor da nova linha é maior que a linha anterior
+# Caso seja, esse valor é armazenado
+# Fazendo assim no final mostrar o maior valor
+# Mostar os resultados
+# Setar os textos
+
+# Plotar os graficos
+
+# Relação ESTADO x FATURAMENTO
+# Relação ESTADO X LUCRO
