f6908bc12ae9177c388dc2e6c8f33b2dc4881ec5
Guilherme Ávila <90806129+guilhermeavila77@users.noreply.github.com>
2022-06-30 13:10:19 -0300

    Add files via upload

--- sistema.py
+++ sistema.py
@@ -1,6 +1,64 @@
 from tela_add import *
 import PySimpleGUI as sg
 
+def atualizar():
+    # Variaveis globais
+    global caminho
+    caminho = "Base de de dados (1).xlsx"
+    global tabela
+    tabela = pd.read_excel(caminho)
+    global txtFat
+    global txtDesp
+    global txtLuc
+
+    # Cria as variaveis utilizadas para a busca e as "seta" como zero
+
+    # As buscas são
+
+    # Maior faturamento, maior despesa, maior lucro
+
+    fatanterior = 0
+    despanterior = 0
+    lucroanterior = 0
+
+    # Laços de repetição que procuram as informações
+    # A cada linha ele verifica se o valor da nova linha é maior que a linha anterior
+    # Caso seja, esse valor é armazenado
+    # Fazendo assim no final mostrar o maior valor
+
+    for faturamento in tabela.itertuples():
+        fat = faturamento.FATURAMENTO
+        if fat > fatanterior:
+            fatanterior = fat
+            namefat = faturamento.NOME
+
+    for despesas in tabela.itertuples():
+        desp = despesas.DESPESAS
+        if desp > despanterior:
+            despanterior = desp
+            namedesp = despesas.NOME
+
+    for lucro in tabela.itertuples():
+        luc = lucro.LUCRO
+        if luc > lucroanterior:
+            lucroanterior = luc
+            namelucro = lucro.NOME
+
+    # Mostar os resultados
+
+    # Setar os textos
+
+    txtfaturamento = "O maior faturamento foi do "
+    txtdespesas = "A maior despesa foi do "
+    txtlucro = "O maior lucro foi do "
+    com = " com "
+
+    txtFat = f'{txtfaturamento}{namefat}{com}{fatanterior}'
+    txtDesp = f'{txtdespesas}{namedesp}{com}{despanterior}'
+    txtLuc = f'{txtlucro}{namelucro}{com}{lucroanterior}'
+
+atualizar()
+
 # cria a interface grafica
 sg.theme('LightBrown6')
 
@@ -8,9 +66,8 @@ collun_center = [
     [sg.Text("SISTEMA DE CONTROLE")],
     [sg.Text("graficoFat"), sg.Text("graficoLuc")],
     [sg.Button("ADD CLIENTE"), sg.Button("ALTERAR INFORMAÇÃO"),
-     sg.Button("EXCLUIR CLIENTE")],
-    [sg.Text("maiorfaturamento"), sg.Text(
-        "maiorfaturamento"), sg.Text("maiorfaturamento")]
+     sg.Button("EXCLUIR CLIENTE"), sg.Button("LISTA CLIENTE")],
+    [sg.Text(txtFat), sg.Text(txtDesp), sg.Text(txtLuc)]
 ]
 
 layout = [[sg.VPush()],
@@ -26,5 +83,8 @@ while True:
 
     if evento == "ADD CLIENTE":
         tela_add()
+
+    if evento == "LISTA CLIENTE":
+        table()
 
 janela.close()
--- tela_add.py
+++ tela_add.py
@@ -2,6 +2,9 @@ import pandas as pd
 from openpyxl import load_workbook
 import PySimpleGUI as sg
 
+global caminho
+caminho = "Base de de dados (1).xlsx"
+
 
 def tela_add():
 
@@ -32,21 +35,37 @@ def tela_add():
             despesas = int(valores['despesas'])
             data = str(valores['data'])
             # Adiciona a linha
-            atualizartabela = load_workbook("Base de de dados (1).xlsx")
+            atualizartabela = load_workbook(caminho)
             aba_ativa = atualizartabela.active
             lucro = 0
             aba_ativa.append(
                 [nome, estado, pais, faturamento, despesas, lucro, data])
             # Salva a planilha e atualizar o data frame
-            atualizartabela.save("Base de de dados (1).xlsx")
-            tabela = pd.read_excel("Base de de dados (1).xlsx")
+            atualizartabela.save(caminho)
+            tabela = pd.read_excel(caminho)
             # Calcula o lucro
             tabela["LUCRO"] = tabela["FATURAMENTO"] - tabela["DESPESAS"]
             # Atualizar e salvar o data frame
-            tabela.to_excel("Base de de dados (1).xlsx", index=False)
-            tabela = pd.read_excel("Base de de dados (1).xlsx")
+            tabela.to_excel(caminho, index=False)
+            tabela = pd.read_excel(caminho)
 
             sg.popup('CLIENTE ADICIONADO!')
             janelaAdd.close()
 
     janelaAdd.close()
+
+
+def table():
+    layoutTable = [
+        [sg.Text("LISTA DE CLIENTES")],
+        [sg.Button("VOLTAR")]
+    ]
+
+    janelaTable = sg.Window("LISTA CLIENTES", layoutTable)
+
+    while True:
+        evento, valores = janelaTable.read()
+        if evento == sg.WIN_CLOSED or evento == "VOLTAR":
+            break
+
+    janelaTable.close()
